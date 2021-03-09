4b447a97af68e6607191dcbb244c068e2d3e073b
Jason <jasonscoppetta@gmail.com>
2021-03-09 15:23:56 +0100

    Added postinstall script and multi token creation

--- utils/buildThemeFromToken.js
+++ utils/buildThemeFromToken.js
@@ -1,42 +1,52 @@
 const fs = require('fs');
+const path = require('path');
 const packageInfo = require('../package.json')
 
-const tokensFolder = '../src/tokens/';
-const tokenName = 'embers-last';
-const newTokenFolder = tokensFolder + tokenName + '/';
-const rawdata = fs.readFileSync(tokensFolder + 'embers-last.json');
-const token = JSON.parse(rawdata);
-const JS_EOL = '\n';
+const tokensFolder = process.cwd() + '/src/tokens/';
+const tokenFileExtension = 'json';
 
-const interfacesFilePath = newTokenFolder + 'interfaces.ts';
-const indexFilePath = newTokenFolder + 'index.ts';
-const themeFilePath = newTokenFolder + 'theme.ts';
+fs.readdir(tokensFolder, (err, files) => {
+    const targetFiles = files.filter(function(file) {
+        return path.extname(file).toLowerCase() === '.'+tokenFileExtension;
+    });
 
-const breakpoints = {};
-const typography = {};
-const typographyNames = [];
+    targetFiles.map((tokenFile) => {
 
-const typographyAllowProperties = ['fontSize', 'fontFamily', 'fontStyle', 'fontWeight', 'letterSpacing', 'lineHeight', 'textDecoration'];
+        const tokenName = path.basename(tokenFile, '.'+tokenFileExtension);
+        const newTokenFolder = tokensFolder + tokenName + '/';
+        const rawdata = fs.readFileSync(tokensFolder + tokenFile);
+        const token = JSON.parse(rawdata);
+        const JS_EOL = '\n';
 
-const theme = {typography:{}, colors:{}, sizes:[], spacing:[]};
+        const interfacesFilePath = newTokenFolder + 'interfaces.ts';
+        const indexFilePath = newTokenFolder + 'index.ts';
+        const themeFilePath = newTokenFolder + 'theme.ts';
 
-const replaceCSSProperties = {
-    backgroundColor: ['iToken.ColorType', 'CSS.Property.BackgroundColor'],
-    color: ['iToken.ColorType', 'CSS.Property.Color'],
-    borderColor: ['iToken.ColorType', 'CSS.Property.BorderColor'],
-    borderLeftColor: ['iToken.ColorType', 'CSS.Property.BorderColor'],
-    borderRightColor: ['iToken.ColorType', 'CSS.Property.BorderColor'],
-    borderTopColor: ['iToken.ColorType', 'CSS.Property.BorderColor'],
-    borderBottomColor: ['iToken.ColorType', 'CSS.Property.BorderColor'],
-}
+        const breakpoints = {};
+        const typography = {};
+        const typographyNames = [];
 
-let TypographySizeTypeContent = '';
-let TypographyTokenContent = '';
-let TextStyleTokenContent = '';
-let ColorsTokenContent = '';
-let ColorTypeContent = '';
-let TypographyTypeContent = '';
-const DesignTokenInterfaceContent = `
+        const typographyAllowProperties = ['fontSize', 'fontFamily', 'fontStyle', 'fontWeight', 'letterSpacing', 'lineHeight', 'textDecoration'];
+
+        const theme = {typography:{}, colors:{}, sizes:[], spacing:[]};
+
+        const replaceCSSProperties = {
+            backgroundColor: ['iToken.ColorType', 'CSS.Property.BackgroundColor'],
+            color: ['iToken.ColorType', 'CSS.Property.Color'],
+            borderColor: ['iToken.ColorType', 'CSS.Property.BorderColor'],
+            borderLeftColor: ['iToken.ColorType', 'CSS.Property.BorderColor'],
+            borderRightColor: ['iToken.ColorType', 'CSS.Property.BorderColor'],
+            borderTopColor: ['iToken.ColorType', 'CSS.Property.BorderColor'],
+            borderBottomColor: ['iToken.ColorType', 'CSS.Property.BorderColor'],
+        }
+
+        let TypographySizeTypeContent = '';
+        let TypographyTokenContent = '';
+        let TextStyleTokenContent = '';
+        let ColorsTokenContent = '';
+        let ColorTypeContent = '';
+        let TypographyTypeContent = '';
+        const DesignTokenInterfaceContent = `
 export interface DesignToken{
     typography: TypographyToken;
     colors: ColorsToken;
@@ -44,7 +54,7 @@ export interface DesignToken{
     spacing: number[];
 }
 `
-const AutoGenerateHeader = `
+        const AutoGenerateHeader = `
 /**
  * This code was generated automatically.
  * 
@@ -54,122 +64,122 @@ const AutoGenerateHeader = `
  * the code is regenerated. 
  * 
  * @generated ${new Date().toLocaleDateString('en', {
-    year: 'numeric',
-    month: 'numeric',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric'
-})}
+            year: 'numeric',
+            month: 'numeric',
+            day: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric'
+        })}
  * @generator ${packageInfo.name} ${packageInfo.version}
  */`;
 
-String.prototype.toCamelCase = function() {
-    return this.replace(/^([A-Z])|\s(\w)/g, function(match, p1, p2, offset) {
-        if (p2) return p2.toUpperCase();
-        return p1.toLowerCase();
-    });
-};
+        String.prototype.toCamelCase = function() {
+            return this.replace(/^([A-Z])|\s(\w)/g, function(match, p1, p2, offset) {
+                if (p2) return p2.toUpperCase();
+                return p1.toLowerCase();
+            });
+        };
 
-function convertUnitSize( unit ){
-    switch(unit){
-        default:
-            return 'px';
-    }
-}
+        function convertUnitSize( unit ){
+            switch(unit){
+                default:
+                    return 'px';
+            }
+        }
 
-if (!fs.existsSync(newTokenFolder)){
-    fs.mkdirSync(newTokenFolder);
-}
+        if (!fs.existsSync(newTokenFolder)){
+            fs.mkdirSync(newTokenFolder);
+        }
 
-// Build breakpoints
-TypographySizeTypeContent += 'export enum TypographySizeType{\n';
-TypographyTokenContent += 'export interface TypographyToken{\n';
-for(const breakPointName in token.sizes){
-    theme.typography[breakPointName] = {};
-    theme.sizes.push([Number(token.sizes[breakPointName].value), breakPointName]);
-    breakpoints[breakPointName] = Number(token.sizes[breakPointName].value);
-    TypographySizeTypeContent += `    '${breakPointName}'='${breakPointName}',\n`;
-    TypographyTokenContent += `    ${breakPointName}: TextStyleToken;\n`;
-}
-TypographySizeTypeContent += '}';
-TypographyTokenContent += '}';
+        // Build breakpoints
+        TypographySizeTypeContent += 'export enum TypographySizeType{\n';
+        TypographyTokenContent += 'export interface TypographyToken{\n';
+        for(const breakPointName in token.sizes){
+            theme.typography[breakPointName] = {};
+            theme.sizes.push([Number(token.sizes[breakPointName].value), breakPointName]);
+            breakpoints[breakPointName] = Number(token.sizes[breakPointName].value);
+            TypographySizeTypeContent += `    '${breakPointName}'='${breakPointName}',\n`;
+            TypographyTokenContent += `    ${breakPointName}: TextStyleToken;\n`;
+        }
+        TypographySizeTypeContent += '}';
+        TypographyTokenContent += '}';
 
-// Build spacing
-for(const spaceName in token.spacing){
-    theme.spacing.push(Number(spaceName));
-}
+        // Build spacing
+        for(const spaceName in token.spacing){
+            theme.spacing.push(Number(spaceName));
+        }
 
-// Build Colors
-ColorsTokenContent += 'export interface ColorsToken{\n';
-ColorTypeContent += 'export type ColorType = string';
-for(const colorName in token.color){
-    ColorsTokenContent += `    ${colorName}: string;\n`;
-    ColorTypeContent += ` | '${colorName}'`;
-    if(token.color[colorName].type==='color'){
-        theme.colors[colorName] = token.color[colorName].value;
-    }
-}
-ColorsTokenContent += '}';
-ColorTypeContent += ';';
+        // Build Colors
+        ColorsTokenContent += 'export interface ColorsToken{\n';
+        ColorTypeContent += 'export type ColorType = string';
+        for(const colorName in token.color){
+            ColorsTokenContent += `    ${colorName}: string;\n`;
+            ColorTypeContent += ` | '${colorName}'`;
+            if(token.color[colorName].type==='color'){
+                theme.colors[colorName] = token.color[colorName].value;
+            }
+        }
+        ColorsTokenContent += '}';
+        ColorTypeContent += ';';
 
-Object.keys(breakpoints).map((sizeName)=>{
-    typography[sizeName] = {};
-    for (const _typoSize in token[sizeName]) {
-        const replaceExp = new RegExp(`^${sizeName} `);
-        const typoSize = _typoSize.replace(replaceExp, '').toCamelCase();
-        const typo = token[sizeName][_typoSize];
-        if(typo.category==='font'){
-            const typoValue = {};
-            typography[sizeName][typoSize] = {};
-            if(!typographyNames.includes(typoSize)) typographyNames.push(typoSize);
-            for(const typoPropertyName in typo){
-                if(typographyAllowProperties.includes(typoPropertyName)){
-                    // @ts-ignore
-                    const typoProperty = typo[typoPropertyName];
-                    if(typoProperty.unit)
-                        typography[sizeName][typoSize][typoPropertyName] = `${typoProperty.value}${convertUnitSize(typoProperty.unit)}`;
-                    else
-                        typography[sizeName][typoSize][typoPropertyName] = typoProperty.value;
+        Object.keys(breakpoints).map((sizeName)=>{
+            typography[sizeName] = {};
+            for (const _typoSize in token[sizeName]) {
+                const replaceExp = new RegExp(`^${sizeName} `);
+                const typoSize = _typoSize.replace(replaceExp, '').toCamelCase();
+                const typo = token[sizeName][_typoSize];
+                if(typo.category==='font'){
+                    const typoValue = {};
+                    typography[sizeName][typoSize] = {};
+                    if(!typographyNames.includes(typoSize)) typographyNames.push(typoSize);
+                    for(const typoPropertyName in typo){
+                        if(typographyAllowProperties.includes(typoPropertyName)){
+                            // @ts-ignore
+                            const typoProperty = typo[typoPropertyName];
+                            if(typoProperty.unit)
+                                typography[sizeName][typoSize][typoPropertyName] = `${typoProperty.value}${convertUnitSize(typoProperty.unit)}`;
+                            else
+                                typography[sizeName][typoSize][typoPropertyName] = typoProperty.value;
+                        }
+                    }
+
+                    theme.typography[sizeName][typoSize] = typography[sizeName][typoSize];
                 }
             }
+        });
 
-            theme.typography[sizeName][typoSize] = typography[sizeName][typoSize];
-        }
-    }
-});
+        TextStyleTokenContent += 'export interface TextStyleToken{\n';
+        TypographyTypeContent += 'export type TypographyType = string';
+        typographyNames.map((typographyName)=>{
+            TextStyleTokenContent += `    ${typographyName}: TypographyCSSComponent;\n`;
+            TypographyTypeContent += ` | '${typographyName}'`;
+        })
+        TextStyleTokenContent += '}';
+        TypographyTypeContent += ';';
 
-TextStyleTokenContent += 'export interface TextStyleToken{\n';
-TypographyTypeContent += 'export type TypographyType = string';
-typographyNames.map((typographyName)=>{
-    TextStyleTokenContent += `    ${typographyName}: TypographyCSSComponent;\n`;
-    TypographyTypeContent += ` | '${typographyName}'`;
-})
-TextStyleTokenContent += '}';
-TypographyTypeContent += ';';
-
-fs.unlink(interfacesFilePath, ()=>{
-    fs.writeFile(interfacesFilePath, '' +
-        'import {TypographyCSSComponent} from "../../libs/embers";' + JS_EOL +
-        AutoGenerateHeader + JS_EOL +
-        TypographySizeTypeContent + JS_EOL +
-        TypographyTokenContent + JS_EOL +
-        TextStyleTokenContent + JS_EOL +
-        TypographyTypeContent + JS_EOL +
-        ColorsTokenContent + JS_EOL +
-        ColorTypeContent + JS_EOL +
-        DesignTokenInterfaceContent + JS_EOL,
-        function(err) {
-        if(err) {
-            return console.log(err);
-        }
-        console.log("Interfaces file saved!");
-    });
-});
-fs.unlink(indexFilePath, ()=>{
-    fs.writeFile(
-        indexFilePath,
-        AutoGenerateHeader + JS_EOL + `
+        fs.unlink(interfacesFilePath, ()=>{
+            fs.writeFile(interfacesFilePath, '' +
+                'import {TypographyCSSComponent} from "../../libs/embers";' + JS_EOL +
+                AutoGenerateHeader + JS_EOL +
+                TypographySizeTypeContent + JS_EOL +
+                TypographyTokenContent + JS_EOL +
+                TextStyleTokenContent + JS_EOL +
+                TypographyTypeContent + JS_EOL +
+                ColorsTokenContent + JS_EOL +
+                ColorTypeContent + JS_EOL +
+                DesignTokenInterfaceContent + JS_EOL,
+                function(err) {
+                    if(err) {
+                        return console.log(err);
+                    }
+                    console.log("Interfaces file saved!");
+                });
+        });
+        fs.unlink(indexFilePath, ()=>{
+            fs.writeFile(
+                indexFilePath,
+                AutoGenerateHeader + JS_EOL + `
 import {CSSProperties} from 'react';
 import * as iToken from './interfaces';
 import * as CSS from 'csstype';
@@ -179,39 +189,43 @@ import theme from './theme'
 export interface DesignTokenCSSProperties extends Omit<CSSProperties, ${Object.keys(replaceCSSProperties).map(propertyName=>`'${propertyName}'`).join(' | ')}>{
     typography?: iToken.TypographyType | iToken.TypographyType[];
 ${Object.keys(replaceCSSProperties).map((propertyName)=>(
-`    ${propertyName}?: ${replaceCSSProperties[propertyName].join(' | ')} | (${replaceCSSProperties[propertyName].join(' | ')})[];`
-)).join('\n')}
+                    `    ${propertyName}?: ${replaceCSSProperties[propertyName].join(' | ')} | (${replaceCSSProperties[propertyName].join(' | ')})[];`
+                )).join('\n')}
 }
 export function emb(cssProperties: DesignTokenCSSProperties, wrapMediaQueries?: boolean): CSSProperties{
     return prepareEmbOutput(cssProperties, theme, wrapMediaQueries);
 }
         `,
-        (err) => {
-            if(err) {
-                return console.log(err);
-            }
-            console.log("Index file saved!");
-        }
-    );
-});
+                (err) => {
+                    if(err) {
+                        return console.log(err);
+                    }
+                    console.log("Index file saved!");
+                }
+            );
+        });
 
 //TODO: Remove ts-ignore after Elena fixes font synch
-fs.unlink(themeFilePath, ()=>{
-    fs.writeFile(
-        themeFilePath,
-        AutoGenerateHeader + JS_EOL +
-`import {DesignToken} from './interfaces';
+        fs.unlink(themeFilePath, ()=>{
+            fs.writeFile(
+                themeFilePath,
+                AutoGenerateHeader + JS_EOL +
+                `import {DesignToken} from './interfaces';
 // @ts-ignore
 const theme: DesignToken = ${JSON.stringify(theme)}
 
 export default theme;
 
 `,
-        (err) => {
-            if(err) {
-                return console.log(err);
-            }
-            console.log("Theme file saved!");
-        }
-    );
+                (err) => {
+                    if(err) {
+                        return console.log(err);
+                    }
+                    console.log("Theme file saved!");
+                }
+            );
+        });
+
+    });
+
 });
