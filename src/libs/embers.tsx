2c21aeb6f1776b362539bbeab6d2ccb049d04782
Jason <jasonscoppetta@gmail.com>
2021-03-09 18:14:22 +0100

    Missing files

--- src/libs/embers.tsx
+++ src/libs/embers.tsx
@@ -2,6 +2,12 @@ import {PropsWithChildren, useContext, useEffect, useState} from "react";
 import throttle from 'lodash.throttle';
 import createThemeProvider, {ThemeProviderType} from "./createThemeProvider";
 import * as React from "react";
+import {EmberIcon} from "../common/common";
+import Icon from "../components/Icon/index";
+
+const radiiProperties = ['borderRadius', 'borderTopLeftRadius', 'borderTopRightRadius', 'borderBottomRightRadius', 'borderBottomLeftRadius'];
+const spacingProperties = ['padding', 'paddingBottom', 'paddingTop', 'paddingLeft', 'paddingRight', 'margin', 'marginTop', 'marginBottom', 'marginLeft', 'marginRight'];
+const colorProperties = ['color', 'background', 'backgroundColor', 'border', 'borderColor', 'backgroundImage'];
 
 const getDeviceConfig = (width: number, sizes: any, asIndex?: boolean): string | number => {
 
@@ -86,8 +92,6 @@ export function createTheme(themes: any){
     }
 }
 
-const colorProperties = ['color', 'backgroundColor', 'borderColor'];
-
 export function prepareEmbOutputWrapperMediaQueries(emberCSSProperties: any, theme: any): any{
     let breakPointName = '';
     let breakPointIndex = -1;
@@ -106,64 +110,70 @@ export function prepareEmbOutputWrapperMediaQueries(emberCSSProperties: any, the
         else if(i === sizes.length-1) cssBreakPointContainer = `@media(min-width: ${sizes[i][0]}px)`;
         else if(i > 0) cssBreakPointContainer = `@media(min-width: ${sizes[i-1][0]}px), @media(max-width: ${sizes[i+1][0]}px)`;
 
-        Object.keys(emberCSSProperties).map((propertyName)=>{
-
-            const rawValue = emberCSSProperties[propertyName];
-            let value = null;
-
-            if(Array.isArray(rawValue)){
-                if(rawValue[breakPointIndex]) value = rawValue[breakPointIndex];
-            }else{
-                value = rawValue;
-            }
-
-            if(propertyName === 'typography'){
-                if(theme.typography[breakPointName][value]){
-                    mediaQueries[cssBreakPointContainer] = {...mediaQueries[cssBreakPointContainer], ...theme.typography[breakPointName][value]};
-                }
-            }else if(colorProperties.includes(propertyName)){
-                if(theme.colors[value]) value = theme.colors[value];
-            }
-
-            mediaQueries[cssBreakPointContainer][propertyName] = value;
-        });
+        mediaQueries[cssBreakPointContainer] = generateCSSProperties(emberCSSProperties, theme, breakPointIndex, breakPointName)
     }
-
-    console.log('asdasdasdadads', mediaQueries);
 
     return mediaQueries;
 }
 
+export function generateCSSProperties(emberCSSProperties: any, theme: any, breakpointIndex: number, breakPointName: string){
+    const deepMap = ( obj: any ) => {
+        let newObj: any = {};
+
+        Object.keys(obj).map((propertyName)=>{
+            const rawValue = obj[propertyName];
+            let value: string | number | [] | null = null;
+
+            if(Array.isArray(rawValue)) {
+                if (rawValue[breakpointIndex]) value = rawValue[breakpointIndex];
+            }else if(typeof rawValue === 'object'){
+                newObj[propertyName] = deepMap(rawValue);
+            }else{
+                value = rawValue;
+            }
+
+            if(value!==undefined&&value!==null){
+                if(propertyName === 'typography'){
+                    if(theme.typography[breakPointName][value as string]){
+                        newObj = {...newObj, ...theme.typography[breakPointName][value as string]};
+                    }
+                }else if(propertyName === 'paddingX'){
+                    newObj = {...newObj, ...{paddingLeft: theme.spacing[value as number], paddingRight: theme.spacing[value as number]}};
+                }else if(propertyName === 'paddingY'){
+                    newObj = {...newObj, ...{paddingTop: theme.spacing[value as number], paddingBottom: theme.spacing[value as number]}};
+                }else if(propertyName === 'marginX'){
+                    newObj = {...newObj, ...{marginLeft: theme.spacing[value as number], marginRight: theme.spacing[value as number]}};
+                }else if(propertyName === 'marginY'){
+                    newObj = {...newObj, ...{marginTop: theme.spacing[value as number], marginBottom: theme.spacing[value as number]}};
+                }else if(radiiProperties.includes(propertyName)){
+                    value = theme.radius[value as number];
+                }else if(spacingProperties.includes(propertyName)){
+                    value = theme.spacing[value as number];
+                }else if(colorProperties.includes(propertyName)){
+                    if(theme.colors[value as string]) value = theme.colors[value as string];
+                    else Object.keys(theme.colors).map((colorName)=>{ value = String(value).replace(colorName, theme.colors[colorName]) })
+                }
+
+                newObj[propertyName] = value;
+            }
+        });
+
+        return newObj;
+    }
+
+    return deepMap(emberCSSProperties);
+}
+
 export function prepareEmbOutput( emberCSSProperties: any, theme: any, wrapMediaQueries: boolean | undefined ): any{
 
-    if(wrapMediaQueries) return prepareEmbOutputWrapperMediaQueries(emberCSSProperties, theme);
+    if(wrapMediaQueries!==false) return prepareEmbOutputWrapperMediaQueries(emberCSSProperties, theme);
 
     const breakpointIndex = getCurrentBreakpointIndex(theme.sizes);
     const breakPointName = getCurrentBreakpoint(theme.sizes);
 
-    let newProperties: any = {};
+    const newProperties = generateCSSProperties(emberCSSProperties, theme, breakpointIndex, breakPointName);
 
-    Object.keys(emberCSSProperties).map((propertyName)=>{
-
-        const rawValue = emberCSSProperties[propertyName];
-        let value = null;
-
-        if(Array.isArray(rawValue)){
-            if(rawValue[breakpointIndex]) value = rawValue[breakpointIndex];
-        }else{
-            value = rawValue;
-        }
-
-        if(propertyName === 'typography'){
-            if(theme.typography[breakPointName][value]){
-                newProperties = {...newProperties, ...theme.typography[breakPointName][value]};
-            }
-        }else if(colorProperties.includes(propertyName)){
-            if(theme.colors[value]) value = theme.colors[value];
-        }
-
-        newProperties[propertyName] = value;
-    });
+    console.log('properties', newProperties);
 
     return newProperties;
 }
@@ -176,4 +186,27 @@ export interface TypographyCSSComponent{
     letterSpacing?: string;
     lineHeight?: string;
     textDecoration?: string;
+}
+
+export const buildClassNames = ( classes: {[key: string]: boolean | string | undefined | null} ) => {
+    let classNames = '';
+    Object.keys(classes).map((cName: string) => {
+        if(classes[cName]===true) classNames += cName + ' ';
+        else if(typeof classes[cName] == 'string') classNames += classes[cName] + ' ';
+    })
+
+    return classNames;
+}
+
+export const embIcon = ( icon: EmberIcon ): any => {
+    if(icon){
+        if(typeof icon === 'string'){
+            return <Icon svgIcon={icon} />;
+        }else{
+            // @ts-ignore
+            return icon();
+        }
+    }
+
+    return <></>;
 }
